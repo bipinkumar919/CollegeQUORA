2f21d565dc316b0e2cb80d3b8c71a3bf36c7c419
Your Name <you@example.com>
2017-08-17 23:47:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bipin/collegequora/MessageAdapter.java
@@ -0,0 +1,72 @@
+package com.example.bipin.collegequora;
+
+/**
+ * Created by bipin on 10/6/17.
+ */
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+/**
+ * Created by bipin on 10/6/17.
+ */
+
+public class MessageAdapter extends ArrayAdapter<FriendlyMessage>  {
+    public MessageAdapter(Context context, int resource, List<FriendlyMessage> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.item_message, parent, false);
+
+        }
+
+        ImageView photoImageView = (ImageView) convertView.findViewById(R.id.photoImageView);
+        TextView messageTextView = (TextView) convertView.findViewById(R.id.messageTextView);
+        TextView authorTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+
+
+
+
+
+
+
+        FriendlyMessage message = getItem(position);
+
+        boolean isPhoto = message.getPhotoUrl() != null;
+        if (isPhoto) {
+            messageTextView.setVisibility(View.GONE);
+            photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(photoImageView.getContext())
+                    .load(message.getPhotoUrl())
+                    .into(photoImageView);
+        } else {
+            messageTextView.setVisibility(View.VISIBLE);
+            photoImageView.setVisibility(View.GONE);
+            messageTextView.setText(message.getText());
+        }
+        authorTextView.setText(message.getName());
+
+        return convertView;
+
+
+
+    }
+
+
+
+}
